11829ca512da48cb85c31aefc911da56eb6916e8
Smadadi2 <shahaghamadadi@gmail.com>
2021-03-22 17:56:08 +0100

    nye filer

---
+++ skrivfile.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+
+int main() {
+
+    int fd, sz;
+
+    fd = open("bar.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) {
+        perror("feil ved skriving av filen"); exit(1);
+    }
+    sz = write(fd, "Heibro\0", 6);
+
+    close(fd);
+}
+
